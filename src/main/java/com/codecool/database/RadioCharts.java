8e01d59bc6e1b0a68c5af9a071989321f228b7c3
Herczeg Csaba <hecsa76@gmail.com>
2021-03-24 17:50:52 +0100

    Thats it

--- src/main/java/com/codecool/database/RadioCharts.java
+++ src/main/java/com/codecool/database/RadioCharts.java
@@ -1,6 +1,16 @@
 package com.codecool.database;
 
-
 public class RadioCharts {
-    //TODO: implement this class
+
+    public RadioCharts(String DB_URL, String DB_USER, String DB_PASSWORD) {
+    }
+
+    public String getMostPlayedSong() {
+        return "";
+    }
+
+    public String getMostActiveArtist() {
+        return "";
+    }
 }
+
